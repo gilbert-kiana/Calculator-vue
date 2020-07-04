ef11b300e5896c4715ebc732ab2db8062ebba5ce
gilbert-kiana <gilbertintabo@gmail.com>
2020-07-04 10:18:18 +0300

    operators fixed

--- src/components/calculator.vue
+++ src/components/calculator.vue
@@ -4,22 +4,22 @@
     <div @click="clear" class="btn">Clear</div>
     <div @click="sign" class="btn">+/-</div>
     <div @click="percent" class="btn">%</div>
-    <div class="btn operator">/</div>
+    <div @click="divide" class="btn operator">/</div>
     <div @click="append('9')" class="btn">9</div>
     <div @click="append('8')" class="btn">8</div>
     <div @click="append('7')" class="btn">7</div>
-    <div class="btn operator">+</div>
+    <div @click="add" class="btn operator">+</div>
     <div @click="append('6')" class="btn">6</div>
     <div @click="append('5')" class="btn">5</div>
     <div @click="append('4')" class="btn">4</div>
-    <div class="btn operator">x</div>
+    <div @click="times" class="btn operator">x</div>
     <div @click="append('3')" class="btn">3</div>
     <div @click="append('2')" class="btn">2</div>
     <div @click="append('1')" class="btn">1</div>
-    <div class="btn operator">-</div>
+    <div @click="minus" class="btn operator">-</div>
     <div @click="append('0')" class="btn zero">0</div>
     <div @click="dot" class="btn">.</div>
-    <div class="btn operator">=</div>
+    <div @click="equal" class="btn operator">=</div>
   </div>
 </template>
 
@@ -28,11 +28,15 @@ export default {
   name: 'calculator',
  data(){
    return{
-     current:''
+     current:'',
+     previous:null,
+     operator:null,
+     operatorClicked:false
      
    }
  },
  methods:{
+   
    clear(){
      this.current='';   
      },
@@ -44,7 +48,13 @@ export default {
        this.current=`${parseFloat(this.current)/100}`;
      },
      append(number){
+       if(this.operatorClicked)
+       {
+         this.current="";
+         this.operatorClicked=false;
+       }
        this.current=`${this.current}${number}`
+       
      },
      dot(){
        if(this.current.indexOf('.') === -1)
@@ -53,6 +63,30 @@ export default {
        }else {
          this.current=`error`
        }
+     },
+     setPrevious(){
+      this.previous=this.current;
+       this.operatorClicked=true;
+   },
+     add(){
+        this.operator= (a,b) => a+b;
+        this.setPrevious();
+     },
+     divide(){
+      this.operator= (a,b) =>a/b;
+      this.setPrevious();
+     },
+     minus(){
+       this.operator=(a,b) =>a-b;
+      this.setPrevious();
+     },
+     times(){
+       this.operator=(a,b) =>a*b;
+       this.setPrevious();
+     },
+     equal(){
+       this.current=`${this.operator(parseFloat(this.current),parseFloat(this.previous))}`
+       this.previous=null;
      }
  }
   
